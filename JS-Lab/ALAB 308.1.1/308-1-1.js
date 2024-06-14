d5fcef7ebbc5f94493cbde6cb4ca141adbe300e9
shahadwildish <shayalobeidi@gmail.com>
2024-06-14 17:34:21 -0400

    Second Commit first assignment part

--- JS-Lab/ALAB 308.1.1/308-1-1.js
+++ JS-Lab/ALAB 308.1.1/308-1-1.js
@@ -1,5 +1,5 @@
 // The initial numbers that must be verified.
-const n1 = 15;
+const n1 = 10;
 const n2 = 15;
 const n3 = 20;
 const n4 = 5;
@@ -14,27 +14,36 @@ const isSum50 = (n1 + n2 + n3 + n4) == 50;
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
-const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+const isTwoOdd = (n1 % 2) //0   EVEN % 2 = 0
++ (n2 % 2) //1  ODD % 2 = 1
++ (n3 % 2) //0 EVEN %2 = 0
++ (n4 % 2) //1 ODD %2 = 1
+>= 2;
 
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+
+//                                              const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25; 
+const isLess25 = n1<25 || n2<25 || n3<25 || n4<25;
+
+
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
-const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4; //TRUE
 
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+const isValid = isSum50 && isTwoOdd && isLess25 && isUnique;
+            //  TRUE && TRUE && TRUE && TRUE
 
 // Finally, log the results.
-console.log(isValid);
+console.log(isValid); //TRUE
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
@@ -44,3 +53,25 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+
+
+/****************************************************************************************************************************************/
+
+const isDiv5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) == 0
+
+/****************************************************************************************************************************************/
+
+const isFirstLarger = (n1 > n4 )
+
+/****************************************************************************************************************************************/
+
+let results = (n2-n1)
+results = (results * n3)
+let remainder = (results % n4)
+
+console.log('The remainder of the devision of the result by the fourth number is : ' + remainder)
+
+/****************************************************************************************************************************************/
+ 
+
